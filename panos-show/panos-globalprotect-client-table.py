518ea32bf1ba6376fdd6f94a11a5333b2b091219
Nick Schmidt <nick.schmidt3@gmail.com>
2020-07-31 17:39:54 -0800

    jenkins found concatenation errors

--- panos-show/panos-globalprotect-client-table.py
+++ panos-show/panos-globalprotect-client-table.py
@@ -282,9 +282,9 @@ print('Generated PAN-OS API Key!')
 
 # Let's try parsing a payload
 xml_payload_globalprotect_summary = get_xml_from_file('globalprotect_summary.xml')
-print(validate_xml_from_string(xml_payload_globalprotect_summary) + 'Read from file!')
+print(str(validate_xml_from_string(xml_payload_globalprotect_summary)) + 'Read from file!')
 xml_payload_globalprotect_summary_detail = get_xml_from_file('globalprotect_summary_detail.xml')
-print(validate_xml_from_string(xml_payload_globalprotect_summary) + ' Read from file!') 
+print(str(validate_xml_from_string(xml_payload_globalprotect_summary)) + ' Read from file!') 
 # TODO - XML linting doesn't appear to work here.
 # Let's try deploying the payload!
 globalprotect_summary = validate_xml_from_string(do_api_get_opcmd_key(session_auth_key, args.api_endpoint, xml_payload_globalprotect_summary, args.k))
